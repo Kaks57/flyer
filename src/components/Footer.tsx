c7e176719512f3718ef821ed06db502851603d71
Kaks57 <kmahtaj57@gmail.com>
2025-04-18 00:40:19 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <div>
             <h3 className="text-2xl font-display font-bold mb-6">
               <img 
-                src="/IMG_1482.jpeg"
+                src="/IMG_1483.jpeg"
                 alt="Ryanol logo" 
                 className="inline h-[1.2em] align-middle"
               />
